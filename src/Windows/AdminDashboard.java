8e94715006ec17743495ce978fc7d95d37420bbe
PC <PC@Enci>
2023-09-20 19:24:29 +0800

    In Development

---
+++ src/Windows/AdminDashboard.java
@@ -0,0 +1,223 @@
+
+package Windows;
+
+import component.Header;
+import component.Menu;
+import event.EventMenuSelected;
+import event.EventShowPopupMenu;
+import form.BorrowBook;
+import form.HistoryBorrow;
+import form.HistoryLibrarian;
+import form.ManagePatron;
+import form.ViewPatron;
+import form.Home;
+import form.Main;
+import form.ManageBook;
+import form.ManageLibrarian;
+import form.ReturnBook;
+import form.ViewBook;
+import form.ViewLibrarian;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import net.miginfocom.swing.MigLayout;
+import org.jdesktop.animation.timing.Animator;
+import org.jdesktop.animation.timing.TimingTarget;
+import org.jdesktop.animation.timing.TimingTargetAdapter;
+import swing.MenuItem;
+import swing.PopupMenu;
+
+
+public class AdminDashboard extends javax.swing.JFrame {
+
+        private MigLayout layout;
+        private Menu menu;
+        private Header header;
+        private Main main;
+        private Animator animator;
+    
+    public AdminDashboard() {
+        initComponents();
+        init();
+    }
+    
+   
+    
+    private void init(){
+        layout = new MigLayout("fill", "0[]0[100%, fill]0", "0[fill, top]0");
+        bg.setLayout(layout);
+        menu = new Menu();
+        header = new Header(this);
+        main = new Main();
+        menu.addEvent(new EventMenuSelected() {
+            @Override
+            public void menuSelected(int menuIndex, int subMenuIndex) {
+                if (menuIndex == 0){
+                    main.showForm(new Home());
+                } else if (menuIndex == 1) {
+                    if (subMenuIndex == 0) {
+                        main.showForm(new ManagePatron());
+                    } else if (subMenuIndex == 1) {
+                        main.showForm(new ViewPatron());
+                    }
+                } else if (menuIndex == 2) {
+                    if (subMenuIndex == 0) {
+                        main.showForm(new ManageLibrarian());
+                    } else if (subMenuIndex == 1) {
+                        main.showForm(new ViewLibrarian());
+                    }
+                } else if (menuIndex == 3) {
+                    if (subMenuIndex == 0) {
+                        main.showForm(new ManageBook());
+                    } else if (subMenuIndex == 1) {
+                        main.showForm(new ViewBook());
+                    } else if (subMenuIndex == 2) {
+                        main.showForm(new BorrowBook());
+                    } else if (subMenuIndex == 3) {
+                        main.showForm(new ReturnBook());
+                    }
+                } else if (menuIndex == 4) {
+                    if (subMenuIndex == 0) {
+                        main.showForm(new HistoryBorrow());
+                    } else if (subMenuIndex == 1) {
+                        main.showForm(new HistoryLibrarian());
+                    }
+                }
+            }
+        });
+        menu.addEventShowPopup(new EventShowPopupMenu() {
+            @Override
+            public void showPopup(Component com) {
+                MenuItem item = (MenuItem)com;
+                PopupMenu popup = new PopupMenu(AdminDashboard.this, item.getIndex(), item.getEventSelected(), item.getMenu().getSubMenu());
+                int x = AdminDashboard.this.getX() + 52;
+                int y = AdminDashboard.this.getY() + com.getY() + 86;
+                popup.setLocation(x, y);
+                popup.setVisible(true);
+            }
+        });
+        menu.initMenuItem();
+        bg.add(menu,"w 230! , spany 2"); //span y 2 cell//
+        bg.add(header, "h 60!, wrap");
+        bg.add(main, "w 100%, h 100%");
+        TimingTarget target = new TimingTargetAdapter() {
+            @Override
+            public void timingEvent(float fraction) {
+                double width;
+                if (menu.isShowMenu()) {
+                    width = 60 + (170 * (1f - fraction));
+                } else {
+                    width = 60 + (170 * fraction);
+                }
+                layout.setComponentConstraints(menu, "w " + width + "!, spany2");
+                menu.revalidate();
+                
+        }   
+            @Override
+            public void end() {
+                menu.setShowMenu(!menu.isShowMenu());
+                menu.setEnableMenu(true);
+            }
+
+        };
+        animator = new Animator(500, target);
+        animator.setResolution(0);
+        animator.setDeceleration(0.5f);
+        animator.setAcceleration(0.5f);
+        header.addMenuEvent(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                if (!animator.isRunning()) {
+                    animator.start();
+                }
+                menu.setEnableMenu(false);
+                if (menu.isShowMenu()) {
+                    menu.hideallMenu();
+                }
+            }
+        });
+        main.showForm(new Home());
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        bg = new javax.swing.JLayeredPane();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        bg.setBackground(new java.awt.Color(245, 245, 245));
+        bg.setOpaque(true);
+
+        javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
+        bg.setLayout(bgLayout);
+        bgLayout.setHorizontalGroup(
+            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1250, Short.MAX_VALUE)
+        );
+        bgLayout.setVerticalGroup(
+            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 800, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+ 
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(AdminDashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(AdminDashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(AdminDashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(AdminDashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new AdminDashboard().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLayeredPane bg;
+    // End of variables declaration//GEN-END:variables
+}
